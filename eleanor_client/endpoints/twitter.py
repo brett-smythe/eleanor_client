4aed9320efdc9afd5eea39884e0550cd8f298d1e
brett <brett@debian-laptop>
2016-07-28 18:00:22 -0700

    Initial commit of eleanor_client

---
+++ eleanor_client/endpoints/twitter.py
@@ -0,0 +1,62 @@
+"""Utility for interacting with eleanor's twitter related endpoints"""
+import json
+
+import requests
+
+from eleanor_client.settings import settings
+
+
+eleanor_url = 'http://{0}:{1}/'.format(
+    settings.eleanor_url, settings.eleanor_port
+)
+
+
+def get_tracked_twitter_users():
+    """Get all currently tracked twitter users"""
+    req_url = '{0}{1}'.format(eleanor_url, 'twitter-tl-users')
+    response = requests.get(req_url)
+    tracked_users = response.json()['twitter_usernames']
+    return tracked_users
+
+
+def track_new_twitter_user(username):
+    """Track a new twitter user in eleanor"""
+    req_url = '{0}{1}'.format(eleanor_url, 'twitter-tl-users')
+    headers = {'content-type': 'application/json'}
+    payload = json.dumps({'twitter_usernames': [username]})
+    requests.post(req_url, headers=headers, data=payload)
+
+
+def add_tweet_data(tweet):
+    """Makes a request to eleanor adding tweet (which should be a dictionary
+    formatted correctly for the request)"""
+    req_url = '{0}{1}'.format(eleanor_url, 'add-tweet-data')
+    headers = {'content-type': 'application/json'}
+    payload = json.dumps({tweet})
+    requests.post(req_url, headers=headers, data=payload)
+
+
+def get_tweet_data(tweet_id):
+    """When given a tweet_id pull the associated tweet data from eleanor, if no
+    tweet is found with the given tweet_id returns None
+    """
+    query_url = 'tweet/{0}'.format(tweet_id)
+    req_url = '{0}{1}'.format(eleanor_url, query_url)
+    response = requests.get(req_url)
+    if response.status_code == 204:
+        return None
+    else:
+        return response.json()
+
+
+def get_username_last_tweet_id(username):
+    """When given a username check for the latest tweet id associated with it
+    if there is no tweet id associated with that username returns None
+    """
+    query_url = 'last-tweet-id/{0}'.format(username)
+    req_url = '{0}{1}'.format(eleanor_url, query_url)
+    response = requests.get(req_url)
+    if response.status_code == 204:
+        return None
+    else:
+        return response.json()['last_tweet_id']
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setuptools for eleanor_client"""
+from setuptools import setup, find_packages
+
+reqs = []
+
+with open('requirements.txt') as inf:
+    for line in inf:
+        line = line.strip()
+        reqs.append(line)
+
+
+setup(
+    name='eleanor-twitter',
+    version='0.1',
+    description='Package for interacting with eleanor service',
+    author='Brett Smythe',
+    author_email='smythebrett@gmail.com',
+    maintainer='Brett Smythe',
+    maintainer_email='smythebrett@gmail.com',
+    packages=find_packages(),
+    install_reqs=reqs
+)

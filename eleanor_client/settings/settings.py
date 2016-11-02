4b938f1dfa663ad622b3c6509cb049f1d613f513
brett <brett@debian-laptop>
2016-11-01 20:26:06 -0700

    Added cfg reader for eleanor_client

--- eleanor_client/settings/settings.py
+++ eleanor_client/settings/settings.py
@@ -1,3 +1,19 @@
 """Settings for eleanor_client"""
-eleanor_url = "192.168.2.50"
+import os
+import ConfigParser
+
+eleanor_url = ''
 eleanor_port = 5000
+
+path_to_here = os.path.abspath(__file__)
+local_settings_path = "{0}/{1}".format(path_to_here, 'local_settings.py')
+if os.path.isfile(local_settings_path):
+    import local_settings
+    eleanor_url = local_settings.eleanor_url
+    eleanor_port = local_settings.eleanor_port
+else:
+    config = ConfigParser.ConfigParser()
+    config.read('/etc/opt/aquatic_services/service_locations.cfg')
+    eleanor_url = config.get('Eleanor', 'ip_address')
+
+
